e83ecbaeadad6e655f2474c96bccd046007c6e97
Christian Bager Bach Houmann <christian@bagerbach.com>
2024-02-16 16:56:23 +0100

    Let's Go! Chapter 6 Middleware

--- lets-go-book/cmd/web/middleware.go
+++ lets-go-book/cmd/web/middleware.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"log/slog"
 	"net/http"
 )
 
@@ -12,6 +14,41 @@ func secureHeaders(next http.Handler) http.Handler {
 		w.Header().Set("X-Content-Type-Options", "nosniff")
 		w.Header().Set("X-Frame-Options", "deny")
 		w.Header().Set("X-XSS-Protection", "0")
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) logRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var (
+			ip     = r.RemoteAddr
+			proto  = r.Proto
+			method = r.Method
+			uri    = r.RequestURI
+		)
+
+		app.logger.Info(
+			"received request",
+			slog.Any("ip", ip),
+			slog.Any("proto", proto),
+			slog.Any("method", method),
+			slog.Any("uri", uri),
+		)
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) recoverPanic(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Because of defer, this runs during the stack unwinding when panics occur
+		defer func() {
+			if err := recover(); err != nil {
+				w.Header().Set("Connection", "close")
+				app.serverError(w, r, fmt.Errorf("%s", err))
+			}
+		}()
 
 		next.ServeHTTP(w, r)
 	})
--- lets-go-book/go.mod
+++ lets-go-book/go.mod
@@ -3,3 +3,5 @@ module github.com/chhoumann/snippetbox
 go 1.22.0
 
 require github.com/go-sql-driver/mysql v1.7.1
+
+require github.com/justinas/alice v1.2.0
