0fa134e770c353a33fa4c33bc2544744320133f7
Christian Bager Bach Houmann <christian@bagerbach.com>
2024-05-21 08:00:50 +0200

    Let's Go Further! Chapter 9.8 - Returning Pagination Metadata

--- lets-go-further-book/internal/data/filters.go
+++ lets-go-further-book/internal/data/filters.go
@@ -50,3 +50,25 @@ func ValidateFilters(v *validator.Validator, f Filters) {
 
 	v.Check(validator.PermittedValue(f.Sort, f.SortSafelist...), "sort", "invalid sort value")
 }
+
+type Metadata struct {
+	CurrentPage  int `json:"current_page,omitempty"`
+	PageSize     int `json:"page_size,omitempty"`
+	FirstPage    int `json:"first_page,omitempty"`
+	LastPage     int `json:"last_page,omitempty"`
+	TotalRecords int `json:"total_records,omitempty"`
+}
+
+func calculateMetadata(totalRecords, page, pageSize int) Metadata {
+	if totalRecords == 0 {
+		return Metadata{}
+	}
+
+	return Metadata{
+		CurrentPage:  page,
+		PageSize:     pageSize,
+		FirstPage:    1,
+		LastPage:     (totalRecords + pageSize - 1) / pageSize,
+		TotalRecords: totalRecords,
+	}
+}
