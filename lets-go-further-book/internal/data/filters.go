c705549bca44faf0ead18173de2ce368059a08e8
Christian Bager Bach Houmann <christian@bagerbach.com>
2024-05-21 07:31:14 +0200

    Let's Go Further! Chapter 9.6 - Sorting Lists

--- lets-go-further-book/internal/data/filters.go
+++ lets-go-further-book/internal/data/filters.go
@@ -1,12 +1,37 @@
 package data
 
-import "greenlight.bagerbach.com/internal/validator"
+import (
+	"strings"
+
+	"greenlight.bagerbach.com/internal/validator"
+)
 
 type Filters struct {
 	Page         int
 	PageSize     int
 	Sort         string
 	SortSafelist []string
+}
+
+func (f Filters) sortColumn() string {
+	for _, safeValue := range f.SortSafelist {
+		if f.Sort == safeValue {
+			return strings.TrimPrefix(f.Sort, "-")
+		}
+	}
+
+	// If the sort parameter is not in the safelist, we panic
+	// This shouldn't happen, as you'd have checked it in the validator
+	// But is a fine failsafe against SQL injection attacks.
+	panic("unsafe sort parameter: " + f.Sort)
+}
+
+func (f Filters) sortDirection() string {
+	if strings.HasPrefix(f.Sort, "-") {
+		return "DESC"
+	}
+
+	return "ASC"
 }
 
 func ValidateFilters(v *validator.Validator, f Filters) {
