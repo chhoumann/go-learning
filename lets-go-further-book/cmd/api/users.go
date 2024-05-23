bcefcb44200fb325a49b9b636fd368b0da8a997d
Christian Bager Bach Houmann <christian@bagerbach.com>
2024-05-23 21:24:46 +0200

    Let's Go Further! Chapter 12 - User Model Setup and Registration

---
+++ lets-go-further-book/cmd/api/users.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+
+	"greenlight.bagerbach.com/internal/data"
+	"greenlight.bagerbach.com/internal/validator"
+)
+
+func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		Name     string `json:"name"`
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	if err := app.readJSON(w, r, &input); err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	user := &data.User{
+		Name:      input.Name,
+		Email:     input.Email,
+		Activated: false,
+	}
+
+	if err := user.Password.Set(input.Password); err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	v := validator.New()
+
+	if data.ValidateUser(v, user); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	if err := app.models.Users.Insert(user); err != nil {
+		switch {
+		case errors.Is(err, data.ErrDuplicateEmail):
+			v.AddError("email", "a user with this email address already exists")
+			app.failedValidationResponse(w, r, v.Errors)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	if err := app.writeJSON(w, http.StatusCreated, envelope{"user": user}, nil); err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
