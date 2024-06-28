ee8eb5bba292d42fbf56d631fec391935d304fb9
Christian Bager Bach Houmann <christian@bagerbach.com>
2024-06-29 01:32:05 +0200

    switch up further app

--- lets-go-further-book/internal/vcs/vcs.go
+++ lets-go-further-book/internal/vcs/vcs.go
@@ -7,23 +7,32 @@ import (
 
 func Version() string {
 	var (
-		time string
+		time     string
 		revision string
 		modified bool
 	)
 
 	bi, ok := debug.ReadBuildInfo()
-	if ok {
-		for _, s := range bi.Settings {
-			switch s.Key {
-			case "vcs.revision":
-				revision = s.Value
-			case "vcs.modified":
-				modified = s.Value == "true"
-			case "vcs.time":
-				time = s.Value
-			}
+	if !ok {
+		return "unknown-build-info-not-available"
+	}
+
+	for _, s := range bi.Settings {
+		switch s.Key {
+		case "vcs.revision":
+			revision = s.Value
+		case "vcs.modified":
+			modified = s.Value == "true"
+		case "vcs.time":
+			time = s.Value
 		}
+	}
+
+	// Add debugging output
+	fmt.Printf("Debug - Time: %s, Revision: %s, Modified: %v\n", time, revision, modified)
+
+	if time == "" && revision == "" {
+		return "unknown-no-vcs-info"
 	}
 
 	if modified {
